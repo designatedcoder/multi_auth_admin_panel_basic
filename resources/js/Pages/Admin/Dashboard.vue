bec70bf7c54e51c7c55ba1b897f83f758ec87109
harri <harri@yahoo.com>
2021-04-04 22:04:21 -0400

    Admin dashboard with route, controller and vue page

---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+    <admin-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Admin Dashboard
+            </h2>
+        </template>
+
+        <div class="px-6 py-2">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi eos iste libero minus beatae culpa suscipit quidem delectus eaque et corporis quasi perferendis nulla, veritatis nesciunt. Fugit, eos minima.
+        </div>
+    </admin-layout>
+</template>
+
+<script>
+    import AdminLayout from '@/Layouts/AdminLayout'
+    export default {
+        components: {
+            AdminLayout
+        }
+    }
+</script>
