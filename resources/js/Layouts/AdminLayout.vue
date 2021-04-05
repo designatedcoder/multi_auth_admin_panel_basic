38dea61a2f1b73568c2cec93c5be5f6d5c97e9a4
harri <harri@yahoo.com>
2021-04-04 22:06:44 -0400

    new layout for admins

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <app-layout>
+        <!-- Page Heading -->
+        <header class="bg-white shadow" v-if="$slots.header">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <slot name="header"></slot>
+            </div>
+        </header>
+        <div class="flex flex-wrap space-y-6 py-6 sm-justify-around sm:space-y-0">
+            <div class="bg-white overflow-hidden shadow-xl py-6 w-full sm:w-2/3 sm:rounded-lg">
+                <slot></slot>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+    export default {
+        components: {
+            AppLayout,
+        }
+    }
+</script>
