5b16953d32d28d5b4dc6bdc602bf908daa95c539
harri <harri@yahoo.com>
2021-04-09 09:51:22 -0400

    Setup Roles\Index.vue and added green/blue button components

---
+++ resources/js/Pages/Admin/Roles/Index.vue
@@ -0,0 +1,54 @@
+<template>
+    <admin-layout>
+        <template #header>
+            <div class="flex justify-between items-center">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Roles
+                </h2>
+                <blue-button href="#" class="text-sm">Create</blue-button>
+            </div>
+        </template>
+
+        <div class="px-6 py-2">
+            <table class="table-auto w-full">
+                <thead>
+                    <tr>
+                        <th class="text-left pl-3">Name</th>
+                        <th class="text-left pl-3">Created</th>
+                        <th class="text-right pr-3">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(role,index) in roles" :key="index" class="text-center hover:bg-gray-600 hover:text-gray-50" :class="{'bg-gray-300': index%2 === 0}">
+                        <td class="text-left capitalize py-3 pl-3">{{ role.name }}</td>
+                        <td class="text-left py-3 pl-3">{{ role.created_at }}</td>
+                        <td class="py-3">
+                            <div class="flex justify-end pr-3">
+                                <green-button href="#" class="text-sm">Edit</green-button>
+                                <form>
+                                    <jet-danger-button type="submit" class="text-sm ml-1">Delete</jet-danger-button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </admin-layout>
+</template>
+
+<script>
+    import AdminLayout from '@/Layouts/AdminLayout'
+    import BlueButton from '@/Components/BlueButton'
+    import GreenButton from '@/Components/GreenButton'
+    import JetDangerButton from '@/Jetstream/DangerButton'
+    export default {
+        props: ['roles'],
+        components: {
+            AdminLayout,
+            BlueButton,
+            GreenButton,
+            JetDangerButton,
+        }
+    }
+</script>
