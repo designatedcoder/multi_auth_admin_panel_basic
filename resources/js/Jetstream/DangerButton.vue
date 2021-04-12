560dd950a2387597c008417fbd8705290e08285d
harri <harri@yahoo.com>
2021-04-11 22:15:00 -0400

    added visit method to danger button component

--- resources/js/Jetstream/DangerButton.vue
+++ resources/js/Jetstream/DangerButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="type" class="inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red active:bg-red-600 disabled:opacity-25 transition">
+    <button :type="type" class="inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red active:bg-red-600 disabled:opacity-25 transition" @click="visit">
         <slot></slot>
     </button>
 </template>
@@ -11,6 +11,16 @@
                 type: String,
                 default: 'button',
             },
+            href: {},
+        },
+        methods: {
+            visit(event) {
+                if (!this.href) {
+                    return
+                }
+                event.preventDefault()
+                this.$inertia.visit(this.href)
+            }
         }
     }
 </script>
